f1daa6128cd938b59bbe7c0aa6a6b0cca4931c8f
emiel44 <emielbibani@gmail.com>
2022-07-29 17:49:21 +0200

    date naar macdonalds

--- Tiled Lines/Tiled Lines.js
+++ Tiled Lines/Tiled Lines.js
@@ -1,15 +1,31 @@
 var canvas = document.querySelector('canvas');
 var context = canvas.getContext('2d');
 
-var size = 640;
-var step = 80;
+
+var size = 320;
+var step = 40;
 var dpr = window.devicePixelRatio;
 canvas.width = size * dpr;
 canvas.height = size * dpr;
 context.scale(dpr, dpr);
 
-context.lineCap = 'round';
-context.lineWidth = 5;
+function kleur(max){
+  return Math.floor(Math.random()*(max+1));
+}
+
+function kleur2(max){
+  return Math.floor(Math.random()*(max+1));
+}
+document.body.style.backgroundColor = `rgb(`+ kleur(255) + `,`+ kleur(255) + `,` + kleur(255) + `)`;
+
+red = kleur(200);
+green = kleur(200);
+blue = kleur(200);
+var kleur1 = `rgb(${red}, ${green}, ${blue})`;
+
+context.lineCap = 'square';
+context.lineWidth = 10;
+context.strokeStyle = kleur1;
 
 function draw(x, y, width, height) {
   var leftToRight = Math.random() >= 0.5;
